3cb0936f9a4fe32b47dc788377122823b2d9b7b8
brunelz <delgado.hebry24@triana.salesianos.edu>
2024-10-08 11:39:32 +0200

    pokedex casi, faltan fondos

--- pokemon-ui-bruno-y-moises/detail.js
+++ pokemon-ui-bruno-y-moises/detail.js
@@ -1,45 +1,74 @@
-  $(document).ready(function () {
-    var urlParams = new URLSearchParams(window.location.search);
-    var pokemonId = urlParams.get("id");
+$(document).ready(function () {
+  var urlParams = new URLSearchParams(window.location.search);
+  var pokemonId = urlParams.get("id");
 
-    if (pokemonId == null) {
-      alert("No se ha recibido el ID de pokemon");
-      return;
+  if (pokemonId == null) {
+    alert("No se ha recibido el ID de pokemon");
+    return;
+  }
+
+  function changeBackgroundImage(type) {
+    const backgroundImageUrls = {
+      grass: 'url("fondoplanta.jpg")',
+      poison: 'url("fondoveneno.jpg")',
+      fire: 'url("fondofuego.jpg")',
+      water: 'url("fondoagua.jpg")',
+      bug: 'url(fondobicho.jpg)',
+      flying: 'url("fondovolador.jpg")',
+      // Añade más tipos y sus respectivas imágenes de fondo aquí
+    };
+
+    const backgroundImageUrl = backgroundImageUrls[type] || 'url("default-background.jpg")';
+    $('.background').css('background-image', backgroundImageUrl);
+  }
+
+  // Llama a la función changeBackgroundImage con el tipo del Pokémon
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
+    method: 'GET',
+    success: function (data) {
+      const primaryType = data.types[0].type.name;
+      changeBackgroundImage(primaryType);
+    },
+    error: function () {
+      alert("No se pudo obtener los detalles del Pokémon");
     }
+  });
 
-    // Fetch Pokémon details from the API
-    $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
-      method: 'GET',
-      success: function (data) {
-        // Fetch species details for additional information
-        $.ajax({
-          url: data.species.url,
-          method: 'GET',
-          success: function (speciesData) {
-            // Fetch evolution chain
-            $.ajax({
-              url: speciesData.evolution_chain.url,
-              method: 'GET',
-              success: function (evolutionData) {
-                var evolutions = [];
-                var evoChain = evolutionData.chain;
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
+    method: 'GET',
+    success: function (data) {
+      $.ajax({
+        url: data.species.url,
+        method: 'GET',
+        success: function (speciesData) {
+          $.ajax({
+            url: speciesData.evolution_chain.url,
+            method: 'GET',
+            success: function (evolutionData) {
+              var evolutions = [];
+              var evoChain = evolutionData.chain;
 
-                do {
-                  var evoDetails = evoChain['evolves_to'];
-                  evolutions.push({
-                    "species_name": evoChain.species.name,
-                    "url": evoChain.species.url
-                  });
+              do {
+                var evoDetails = evoChain['evolves_to'];
+                evolutions.push({
+                  "species_name": evoChain.species.name,
+                  "url": evoChain.species.url
+                });
 
-                  evoChain = evoDetails[0];
-                } while (!!evoChain && evoChain.hasOwnProperty('evolves_to'));
+                evoChain = evoDetails[0];
+              } while (!!evoChain && evoChain.hasOwnProperty('evolves_to'));
 
-                // Create a template for Pokémon details
-                var types = data.types.map(typeInfo => `<span class="${typeInfo.type.name}">${typeInfo.type.name.toUpperCase()}</span>`).join(' ');
-                var abilities = data.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ');
+              var types = data.types.map(function (typeInfo) {
+                return '<span class="' + typeInfo.type.name + '">' + typeInfo.type.name.toUpperCase() + '</span>';
+              }).join(' ');
 
-                var template = `
+              var abilities = data.abilities.map(function (abilityInfo) {
+                return abilityInfo.ability.name;
+              }).join(', ');
+
+              var template = `
                   <section class="pokemon-info container">
                     <img src="${data.sprites.front_default}" alt="${data.name}">
                     <div class="pokemon-details">
@@ -52,36 +81,42 @@
                         <li>Altura: ${data.height / 10}m</li>
                         <li><strong>Peso:</strong> ${data.weight / 10}kg</li>
                         <li>Sexo: ♂ ♀</li>
-                        <li>Categoría: ${speciesData.genera.find(genus => genus.language.name === 'es').genus}</li>
+                        <li>Categoría: ${speciesData.genera.find(function (genus) {
+                return genus.language.name === 'es';
+              }).genus}</li>
                         <li>Habilidad: ${abilities}</li>
                       </ul>
                     </div>
                   </section>
                   <section class="container evoluciones-container">
                     <div class="evoluciones">
-                      ${evolutions.map(evo => `<div class="evolucion"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evo.species_name}.png" alt="${evo.species_name}"></div>`).join('')}
+                      ${evolutions.map(function (evo) {
+                var evoId = evo.url.split('/').filter(Boolean).pop();
+                return '<div class="evolucion"><a href="detail.html?id=' + evoId + '"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + evoId + '.png" alt="' + evo.species_name + '"></a></div>';
+              }).join('')}
                     </div>
                     <div class="bulbasaur-text">
-                      <p>${speciesData.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text}</p>
+                      <p>${speciesData.flavor_text_entries.find(function (entry) {
+                return entry.language.name === 'es';
+              }).flavor_text}</p>
                     </div>
                   </section>
                 `;
 
-                // Append the template to a container element
-                $('#pokemon-container').html(template);
-              },
-              error: function () {
-                alert("No se pudo obtener la cadena de evolución del Pokémon");
-              }
-            });
-          },
-          error: function () {
-            alert("No se pudo obtener los detalles de la especie del Pokémon");
-          }
-        });
-      },
-      error: function () {
-        alert("No se pudo obtener los detalles del Pokémon");
-      }
-    });
+              $('#pokemon-container').html(template);
+            },
+            error: function () {
+              alert("No se pudo obtener la cadena de evolución del Pokémon");
+            }
+          });
+        },
+        error: function () {
+          alert("No se pudo obtener los detalles de la especie del Pokémon");
+        }
+      });
+    },
+    error: function () {
+      alert("No se pudo obtener los detalles del Pokémon");
+    }
   });
+});
--- pokemon-ui-bruno-y-moises/index.js
+++ pokemon-ui-bruno-y-moises/index.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
+  fetchPokemonData();
+
   $('#btn-get-data').click(function() {
       fetchPokemonData();
   });
 
   function fetchPokemonData() {
       $.ajax({
-          url: 'https://pokeapi.co/api/v2/pokemon?limit=151', // Puedes cambiar el límite según tus necesidades
+          url: 'https://pokeapi.co/api/v2/pokemon?limit=151',
           method: 'GET',
           success: function(response) {
-              console.log('Fetched Pokémon list:', response.results); // Log para depurar
+              console.log('Fetched Pokémon list:', response.results);
+
+              response.results.sort(function(a, b) {
+                  var idA = parseInt(a.url.split('/').slice(-2, -1)[0]);
+                  var idB = parseInt(b.url.split('/').slice(-2, -1)[0]);
+                  return idA - idB;
+              });
+
               displayPokemon(response.results);
           },
           error: function(error) {
@@ -18,36 +27,51 @@ $(document).ready(function() {
   }
 
   function displayPokemon(pokemonList) {
-      const dataContent = $('#data-content');
-      dataContent.empty(); // Limpiar el contenido anterior
+      var dataContent = $('#data-content');
+      dataContent.empty();
 
-      pokemonList.forEach(pokemon => {
-          $.ajax({
+      var pokemonPromises = pokemonList.map(function(pokemon) {
+          return $.ajax({
               url: pokemon.url,
-              method: 'GET',
-              success: function(pokemonData) {
-                  console.log('Fetched Pokémon data:', pokemonData); // Log para depurar
-                    const pokemonCard = `
-                      <div class="pokemon-row">
-                        <a href="detail.html?id=${pokemonData.id}" class="pokemon-card">
-                          <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
-                          <div class="pokemon-info">
-                            <div class="pokemon-name">${capitalizeFirstLetter(pokemonData.name)}</div>
-                            <div class="pokemon-number">#${pokemonData.id.toString().padStart(3, '0')}</div>
-                          </div>
-                          <div class="pokemon-types">
-                            ${pokemonData.types.map(typeInfo => `<span class="type-badge ${typeInfo.type.name}">${capitalizeFirstLetter(typeInfo.type.name)}</span>`).join('')}
-                          </div>
-                        </a>
-                      </div>
-                    `;
-                  dataContent.append(pokemonCard);
-              },
-              error: function(error) {
-                  console.error('Error fetching Pokémon data:', error);
-              }
+              method: 'GET'
           });
       });
+
+      Promise.all(pokemonPromises)
+          .then(function(pokemonDataArray) {
+              var rowContent = '';
+              pokemonDataArray.forEach(function(pokemonData, index) {
+                  console.log('Fetched Pokémon data:', pokemonData);
+                  var pokemonCard = `
+                      <div class="pokemon-card" data-id="${pokemonData.id}">
+                        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" class="pokemon-image">
+                        <div class="pokemon-info">
+                          <div class="pokemon-name">${capitalizeFirstLetter(pokemonData.name)}</div>
+                          <div class="pokemon-number">#${pokemonData.id.toString().padStart(3, '0')}</div>
+                        </div>
+                        <div class="pokemon-types">
+                          ${pokemonData.types.map(function(typeInfo) {
+                              return '<span class="type-badge ' + typeInfo.type.name + '">' + capitalizeFirstLetter(typeInfo.type.name) + '</span>';
+                          }).join('')}
+                        </div>
+                      </div>
+                    `;
+                  rowContent += pokemonCard;
+
+                  if ((index + 1) % 3 === 0 || index === pokemonDataArray.length - 1) {
+                      dataContent.append('<div class="pokemon-row">' + rowContent + '</div>');
+                      rowContent = '';
+                  }
+              });
+
+              $('.pokemon-card').click(function() {
+                  var pokemonId = $(this).data('id');
+                  window.location.href = 'detail.html?id=' + pokemonId;
+              });
+          })
+          .catch(function(error) {
+              console.error('Error fetching Pokémon data:', error);
+          });
   }
 
   function capitalizeFirstLetter(string) {
