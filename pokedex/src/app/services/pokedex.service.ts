b5672bbbcffe5cd0832ef82c4e1c3b47dc2d773c
Dorado <dorado.gumoi24@triana.salesianos.edu>
2024-10-23 12:27:27 +0200

    pilila

--- pokedex/src/app/services/pokedex.service.ts
+++ pokedex/src/app/services/pokedex.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { PokedexResponse } from '../models/pokedex.interface';
 
 @Injectable({
@@ -10,8 +10,8 @@ export class PokedexService {
 
   constructor(private http: HttpClient) { }
 
-  getPoxedex(): Observable<PokedexResponse> {
-    return this.http.get('https://pokeapi.co/api/v2/pokemon/') as Observable<PokedexResponse>;
-}
+  getPokedex(limit: number): Observable<PokedexResponse> {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`) as Observable<PokedexResponse>;
+  }
 
 }
